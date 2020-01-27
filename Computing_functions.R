d14d6f5189b7d3f2b511a110a28d7861e73a48a7
Arthur Leroy <36625932+ArthurLeroy@users.noreply.github.com>
2020-01-27 14:28:51 +0100

    Add files via upload

--- Computing_functions.R
+++ Computing_functions.R
@@ -1,40 +1,37 @@
 library(tidyverse)
+library(optimr)
+library(mvtnorm)
 
 ##### USEFUL FUNCTIONS ########
 '%notin%' <- Negate('%in%')
-###############################
-
 
 ##### KERNELS DEFINITION ######
-kernel = function(t1, t2, theta = list(1, 0.2))
+kernel = function(t1, t2, theta = list(1, 0.5, 0.5))
 { ## t1 : first observational timestamp
   ## t2 : second observatinal timestamp
   ## theta : list of hyperparamaters of the kernel
   ## return : the value of dot production <f(t1),f(t2)> computed from the kernel
   a = theta[[1]] 
   b = theta[[2]]
+  c = 0
   
-  t = 0
-  ##if(t1 == t2){t = 0.1}  t is the value of pointwise variance (i.e. sigma)
-  kern = a * exp((-1/(2 * b)) * t((t1 - t2)) * (t1 - t2))  + t
+  if(all(t1 == t2)){c = exp(theta[[3]])}  #t is the value of pointwise variance (i.e. sigma)
+  kern = exp(a) * exp((-1/(2 * exp(b))) * t((t1 - t2)) * (t1 - t2)) + c
   return(kern)
 }
 
 #kernel2 = function(t1 , t2, theta)
-{ ## t1 : first observational timestamp
+#{ ## t1 : first observational timestamp
   ## t2 : second observatinal timestamp
   ## theta : list of hyperparamaters of the kernel
   ## return : the value of dot production <f(t1),f(t2)> computed from the kernel
-  a = theta[[1]] 
-  b = theta[[2]]
-  
-  t = 0
-  if(t1 == t2){t = 0.1}
-  kern = a * exp((-1/(2 * b)) * t((t1 - t2)) * (t1 - t2)) + t
-  return(kern)
-}
+#  a = theta[[1]] 
+#  b = theta[[2]]
+#  kern = a^2 * exp((-1/(2 * b^2)) * t((t1 - t2)) %*% (t1 - t2))
+#  return(kern)
+#  }
 
-kern_to_cov = function(x, kern = kernel, theta = list(1, 0.2))
+kern_to_cov = function(x, kern = kernel, theta = list(1, 0.2, 0.4))
 { ## x : vector or tibble of all timestamps for each individuals (input vector). 2 columns, 'ID' and 'Timestamp' required
   ## kern : indicates which kernel function to use to compute the covariance matrix
   ## theta : list of the required hyperparameters
@@ -43,7 +40,7 @@ kern_to_cov = function(x, kern = kernel, theta = list(1, 0.2))
   
   ## If user uses kern_to_cov() directly with an observation's vector for one individual
   if(is.vector(x))
-  { mat = sapply(x, function(t) sapply(x, function(s) kernel(t, s, theta = theta)))
+  { mat = sapply(x, function(t) sapply(x, function(s) kern(t, s, theta = theta)))
     if(mat %>% dim() %>% is.null()){mat = as.matrix(mat)}
     rownames(mat) = paste0('X', x)
     colnames(mat) = paste0('X', x)
@@ -53,7 +50,7 @@ kern_to_cov = function(x, kern = kernel, theta = list(1, 0.2))
   ## If the tibble is composed of observations from only one individual
   if(x %>% pull(ID) %>% unique() %>% length() == 1)
   {
-    mat = sapply(x$Timestamp, function(t) sapply(x$Timestamp, function(s) kernel(t, s, theta = theta)))
+    mat = sapply(x$Timestamp, function(t) sapply(x$Timestamp, function(s) kern(t, s, theta = theta)))
     if(mat %>% dim() %>% is.null()){mat = as.matrix(mat)}
     rownames(mat) = paste0('X', x$Timestamp)
     colnames(mat) = paste0('X', x$Timestamp)
@@ -66,7 +63,7 @@ kern_to_cov = function(x, kern = kernel, theta = list(1, 0.2))
     for(i in unique(x$ID))
     {
       indiv = x %>% filter(ID == i) %>% pull(Timestamp)
-      mat = sapply(indiv, function(t) sapply(indiv, function(s) kernel(t, s, theta = theta)))
+      mat = sapply(indiv, function(t) sapply(indiv, function(s) kern(t, s, theta = theta)))
       if(mat %>% dim() %>% is.null()){mat = as.matrix(mat)}
       rownames(mat) = paste0('X', indiv)
       colnames(mat) = paste0('X', indiv)
@@ -76,7 +73,7 @@ kern_to_cov = function(x, kern = kernel, theta = list(1, 0.2))
   } 
 }
 
-kern_to_inv = function(x, kern = kernel, theta = list(1, 0.2))
+kern_to_inv = function(x, kern = kernel, theta = list(1, 0.2, 0.4))
 { ## x : vector or tibble of all timestamps for each individuals (input vector). 2 columns, 'ID' and 'Age' required
   ## kern : indicates which kernel function to use to compute the covariance matrix
   ## theta : list of the required hyperparameters
@@ -86,7 +83,7 @@ kern_to_inv = function(x, kern = kernel, theta = list(1, 0.2))
   ## If user uses kern_to_inv() directly with an observation's vector for one individual
   if(is.vector(x))
   { 
-    mat = sapply(x, function(t) sapply(x, function(s) kernel(t, s, theta = theta))) %>% solve()
+    mat = sapply(x, function(t) sapply(x, function(s) kern(t, s, theta = theta))) %>% solve()
     if(mat %>% dim() %>% is.null()){mat = as.matrix(mat)}
     rownames(mat) = paste0('X', x)
     colnames(mat) = paste0('X', x)
@@ -96,7 +93,7 @@ kern_to_inv = function(x, kern = kernel, theta = list(1, 0.2))
   ## If the tibble is composed of observations from only one individual
   if(x %>% pull(ID) %>% unique() %>% length() == 1)
   {
-    mat = sapply(x$Timestamp, function(t) sapply(x$Timestamp, function(s) kernel(t, s, theta = theta))) %>% solve()
+    mat = sapply(x$Timestamp, function(t) sapply(x$Timestamp, function(s) kern(t, s, theta = theta))) %>% solve()
     if(mat %>% dim() %>% is.null()){mat = as.matrix(mat)}
     rownames(mat) = paste0('X', x$Timestamp)
     colnames(mat) = paste0('X', x$Timestamp)
@@ -109,7 +106,7 @@ kern_to_inv = function(x, kern = kernel, theta = list(1, 0.2))
     for(i in unique(x$ID))
     {
       indiv = x %>% filter(ID == i) %>% pull(Timestamp)
-      mat = sapply(indiv, function(t) sapply(indiv, function(s) kernel(t, s, theta = theta))) %>% solve()
+      mat = sapply(indiv, function(t) sapply(indiv, function(s) kern(t, s, theta = theta))) %>% solve()
       if(mat %>% dim() %>% is.null()){mat = as.matrix(mat)}
       rownames(mat) = paste0('X', indiv)
       colnames(mat) = paste0('X', indiv)
@@ -118,49 +115,58 @@ kern_to_inv = function(x, kern = kernel, theta = list(1, 0.2))
   return(list_mat)
   } 
 }
-###############################
 
 ##### LOGLIKELIHOOD FUNCTIONS ####
-logL = function(y = , t = , theta_i = , sigma = , kern = kernel)
+logL = function(y , t , theta_i, sigma , kern = kernel)
 {
   cov = kern_to_cov(t, kern = kern, theta = theta_i)
-  return(-1/2 * t(y) %*% solve(cov) %*% y - 1/2 %*% det(cov) )
+  return(1/2 * t(y) %*% solve(cov) %*% y + 1/2 %*% det(cov))
 }
-###############################
 
-##### EM FUNCTIONS ###########
+##### EM FUNCTIONS ############
 e_step_GP = function(t, kern, theta)
 {
-   return(kern_to_inv(t, kern = kern, theta = theta))
+   return(kern_to_inv(t, kern = kern, theta))
 }
 
 m_step_GP = function(mean, inv)
 {
   
 }
-##############################
-
 
 #### EM MEAN GP FUNCTIONS ####
-update_inv = function(old_inv, list_inv_i)
+update_inv = function(prior_inv, list_inv_i)
 {
+  new_inv = prior_inv
   
+  for(x in list_inv_i)
+  {
+    inv_i = x
+    common_times = intersect(row.names(x), row.names(new_inv))
+    new_inv[common_times, common_times] = new_inv[common_times, common_times] + inv_i[common_times, common_times]
+  }
   return(new_inv)
 }
 
-update_mean = function(old_mean, old_inv, list_value_i)
+update_mean = function(prior_mean, prior_inv, list_inv_i, list_value_i)
 {
-  weighted_m = old_inv %*% old_mean
-    
-  for(i in list_value_i)
+  if(length(prior_mean) == 1){prior_mean = rep(prior_mean, ncol(prior_inv))}
+  weighted_mean = prior_inv %*% prior_mean
+  #row.names(weithed_mean) = row.names(prior_inv)
+
+  for(j in 1:length(list_value_i)) 
   {
-   # sum de tous les indices communs
+    weighted_i = list_inv_i[[j]] %*% list_value_i[[j]]
+    #row.names(weithed_i) = row.names(list_inv_i[[j]])
+    
+    common_times = intersect(row.names(weighted_i), row.names(weighted_mean))
+    
+    weighted_mean[common_times,] = weighted_mean[common_times,] + weighted_i[common_times,]
   }
-  return(weighted_m)
+  return(weighted_mean)
 }
 
 m_step_meanGP = function(mean, cov)
 {
   return(new_theta_0)
 }
-##############################
---
+++ Test_simple_GP.R
@@ -0,0 +1,111 @@
+setwd(dir = 'C:/Users/user/CloudStation/Maths/These/Processus Gaussiens/Code R/Algo multitask GP')
+source('Computing_functions.R')
+source('Algo_multitask_GP.R')
+
+RMSE = function(m, o){sqrt(mean((m - o)^2))}
+
+########### Package gptk####
+library(gptk)
+
+
+
+
+## Inputs
+x = db_test$Timestamp %>% as.matrix()
+xtest = matrix(seq(9,21, 0.03),ncol=1)
+## Generate y
+
+options = gpOptions()
+options$kern$comp = list("rbf","white")
+
+trueKern = kernCreate(x, list(type="cmpnd",comp=list("rbf", "white")))
+K = kernCompute(trueKern, x)
+
+y = db_test$Output %>% as.matrix()
+y = scale(y,scale=FALSE)
+
+model = gpCreate(dim(x)[2], dim(y)[2], x, y, options)	
+model = gpOptimise(model, display=TRUE, iters=400)
+
+ll_opt = gpLogLikelihood(model)
+
+meanVar = gpPosteriorMeanVar(model, xtest, varsigma.return=TRUE) ## GP mean and variance.
+
+plot(x, y, xlim=c(9,21))
+lines(xtest, meanVar$mu)
+lines(xtest, meanVar$mu+sqrt(meanVar$varsigma), col="blue")
+lines(xtest, meanVar$mu-sqrt(meanVar$varsigma), col="blue")
+
+graphics.off();
+
+########### Gradients of the logL GP #####
+deriv1 = function(t1, t2, theta = list(1, 0.5, 0.5))
+{
+  a = theta[[1]]
+  b = theta[[2]]
+  #c = theta[[3]]
+  
+  return( 2 * a * exp((-1/(2 * b^2)) * t((t1 - t2)) %*% (t1 - t2)) )
+}
+
+deriv2 = function(t1, t2, theta = list(1, 0.5, 0.5))
+{
+  a = theta[[1]]
+  b = theta[[2]]
+  # c = theta[[3]]
+  
+  return( a^2 *  t((t1 - t2)) %*% (t1 - t2) * exp((-1/(2 * b^2)) * t((t1 - t2)) %*% (t1 - t2)) / (b^3) )
+}
+
+deriv3 = function(t1, t2, theta = list(1, 0.5, 0.5))
+{
+  if(all(t1 == t2))
+  {
+    return(2 * theta[[3]])
+  }
+  else{return(0)}
+}
+
+gr_fn = function(x, db = db_obs)
+{
+  y = db$Output
+  t = db$Timestamp
+  inv = kern_to_inv(t, kernel, theta = x)
+  
+  cste_term = inv %*% y %*% t(inv %*% y) - inv
+  
+  g_1 = (-1/2 * cste_term %*% kern_to_inv(t, deriv1, as.list(x))) %>%  diag() %>% sum()
+  g_2 = (-1/2 * cste_term %*% kern_to_inv(t, deriv2, as.list(x))) %>%  diag() %>% sum()
+  #g_3 = (-1/2 * cste_term %*% kern_to_inv(t, deriv3, as.list(x))) %>%  diag() %>% sum()
+  
+  grad = c(g_1, g_2)
+  return(grad)
+}
+
+
+############ logL of the GP ##############
+fn<- function (x, db = db_test, kern = kernel) 
+{
+  #y = db$Output
+  #t = db$Timestamp
+  #n = nrow(db)
+  #inv = kern_to_inv(t, kernel, theta = x)
+  #return(-(t(y) %*% inv %*% y)/2 + log(det(inv))/2 - log(2*pi)*n/2) 
+  return(- dmvnorm(db$Output, rep(0, nrow(db)), 
+                   kern_to_cov(db$Timestamp, kern, theta = x), log = T))
+}
+
+t = 10:20
+db_test = tibble('Timestamp' = t, 
+                 'Input' = paste0('X', t),
+                 'Output' = rmvnorm(1, rep(0,length(t)), kern_to_cov(t, kernel, theta = log(c(15,5, 0.5)))) %>% as.vector())
+
+meth0 <- c("Nelder-Mead", "BFGS", "L-BFGS-B")
+st0 <- c(10, 5, 1.5) %>% log() # the standard start
+result0 <- opm(st0, fn, method= meth0) 
+summary(result0, order=value)
+
+pred_gp(db_test, timestamps = seq(9,21, 0.03), theta = c(result0$p1[1], result0$p1[2], result0$p1[3])) %>% 
+  plot_gp(data = db_test)
+
+
